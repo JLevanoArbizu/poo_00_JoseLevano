a424ffcc15160a743866760817b85a533af75444
JLevanoArbizu <jlevanoabrizu@gmail.com>
2024-09-02 08:32:25 -0500

    1re commit - proyecto

---
+++ poo_00_JoseLevano/src/main/java/com/mycompany/poo_00_joselevano/Poo_00_JoseLevano.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.poo_00_joselevano;
+
+/**
+ *
+ * @author joseLevanoArbizu
+ */
+public class Poo_00_JoseLevano {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
